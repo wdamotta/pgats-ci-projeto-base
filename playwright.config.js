5b15acf7ece8dd312fd7d55334e4039baa0ed4af
Weslley Da Motta <wdamotta@gmail.com>
2024-11-09 22:58:30 -0300

    ci: adding local host url

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://pgats-ci-example.netlify.app',
+    baseURL: process.env.URL || 'https://pgats-ci-example.netlify.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
